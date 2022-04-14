3d8af01ed0eab279f7bd13ab8fb53e884c93ecc6
Floris van Schouwenburg <florisherman@gmail.com>
2022-04-14 12:39:08 +0200

    init

---
+++ src/AppBody.tsx
@@ -0,0 +1,52 @@
+import { BaseSyntheticEvent, useState } from "react";
+
+
+function AppBody() {
+  const [item, setItem] = useState("");
+
+  const [todoItems, addItems] = useState([] as string[])
+
+  function handelChange(event : BaseSyntheticEvent) {
+    const { value } = event.target;
+    setItem(value);
+  }
+
+  function addToList() {
+    addItems(prevItems =>{
+        return[...prevItems, item];
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input
+          name="todoItem"
+          value={item}
+          onChange={handelChange}
+          type="text"
+          placeholder="enter item here"
+        />
+        <button onClick={addToList}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+            {todoItems.map(todoItem => { return(
+            
+            <li>{todoItem}</li>
+            
+            
+            )}) }
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+
+export default AppBody;
